6d9b4131f64692eed5a0b38357a6d50eff2e2097
Nikita Kodenko <kodenkonikita@gmail.com>
2020-01-19 21:32:39 +0700

    Reformatted with Black

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from yaml import load, Loader
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--stage', help='Path to dvc stage file')
-    parser.add_argument('--repo', help='Path to dvc repository')
+    parser.add_argument("--stage", help="Path to dvc stage file")
+    parser.add_argument("--repo", help="Path to dvc repository")
     args = parser.parse_args()
     stage = args.stage
     repo = args.repo
@@ -18,25 +18,28 @@ def main():
     if not os.path.exists(repo):
         raise ValueError("File '{}' doesn't exist".format(repo))
 
-    with open(stage, 'r') as f:
+    with open(stage, "r") as f:
         content = load(f, Loader=Loader)
-    outs = content['outs']
+    outs = content["outs"]
     count = 0
     for i in outs:
-        md5sum = i['md5']
-        outs_path = i['path']
-        cache = i['cache']
+        md5sum = i["md5"]
+        outs_path = i["path"]
+        cache = i["cache"]
         if not cache:
             continue
         path = os.path.join(repo, md5sum[0:2], md5sum[2:])
         if not os.path.exists(path):
-            print("'{}': cached value '{}' not found in dvc remote".
-                  format(outs_path, md5sum))
+            print(
+                "'{}': cached value '{}' not found in dvc remote".format(
+                    outs_path, md5sum
+                )
+            )
             count += 1
 
     if count:
         exit(1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
