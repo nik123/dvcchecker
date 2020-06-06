b3d8f92a324d8aba9dbdcaa9475a075e40fe01c2
Nikita Kodenko <kodenkonikita@gmail.com>
2020-06-06 14:58:54 +0700

    Changed PyYaml version

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name="dvcchecker", packages=find_packages(), install_requires=["PyYAML==5.3"])
+setup(name="dvcchecker", packages=find_packages(), install_requires=["PyYAML>=5.1.2"])
