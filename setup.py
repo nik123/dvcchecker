9a89197ef0c5e8d163ae73f13fd41447ea013cba
Nikita Kodenko <kodenkonikita@gmail.com>
2020-06-06 15:26:50 +0700

    setup.py: entry_points

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
 from setuptools import setup, find_packages
 
-setup(name="dvcchecker", packages=find_packages(), install_requires=["PyYAML>=5.1.2"])
+setup(
+    name="dvcchecker",
+    packages=find_packages(),
+    install_requires=["PyYAML>=5.1.2"],
+    entry_points={"console_scripts": ["dvcchecker = dvcchecker.main:main"]},
+)
