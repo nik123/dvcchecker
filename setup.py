7a78514ff3ae3017a5002ce310d637531590ad2b
nik123 <kodenkonikita@gmail.com>
2021-04-21 15:58:42 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="dvcchecker",
     packages=find_packages(),
-    install_requires=["PyYAML>=5.1.2"],
+    install_requires=["PyYAML>=5.4"],
     entry_points={"console_scripts": ["dvcchecker = dvcchecker.main:main"]},
 )
